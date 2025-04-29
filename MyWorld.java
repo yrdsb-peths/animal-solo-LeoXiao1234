7b126c7788e5304bb4016f2d6783ffd3b01032b0
LeoXiao1 <162858575+LeoXiao1@users.noreply.github.com>
2025-04-29 09:07:46 -0400

    add increase score

--- MyWorld.java
+++ MyWorld.java
@@ -1,17 +1,22 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    Label scoreLabel;
+    public int score = 0;
     public MyWorld() {
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300,350);
-        Label scoreLable = new Label(0,80);
-        addObject(scoreLable,50,50);
+        scoreLabel = new Label(0,80);
+        addObject(scoreLabel,50,50);
         createApple();
 
     }
-    
+    public void increaseScore(){
+        score++;
+        scoreLabel.setValue(score);
+    }
     public void createApple(){
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
